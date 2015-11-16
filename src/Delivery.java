0172b3e2b6e701b67c06b2fc33a4a169a52dd157
Nils Kuhnert <3c7@outlook.com>
2015-11-17 00:18:52 +0100

    Cleanup

--- src/Delivery.java
+++ src/Delivery.java
@@ -5,22 +5,23 @@ public class Delivery {
     private static int xmax, ymax;
     private static boolean[][] map;
     private static char[][] col;
-    private static int[][] disc;
-    private static int[][] fin;
     private static int[][][] prev;
-    private static int time = 0;
-    private static String path = "";
+    private static String path;
 
-    public static String suche(boolean[][] map) {
+    public static String search(boolean[][] map) {
         Delivery.map = map;
         xmax = map.length;
         ymax = map[0].length;
         col = new char[xmax][ymax];
-        disc = new int[xmax][ymax];
-        fin = new int[xmax][ymax];
         prev = new int[xmax][ymax][2];
+        path = "";
 
+        dfs(0, 0, xmax - 1, ymax - 1);
 
+        return backtrackPath(xmax - 1, ymax - 1);
+    }
+
+    public static void dfs(int startx, int starty, int goalx, int goaly) {
         // DFS
         for (int i = 0; i < xmax; i++)
             for (int j = 0; j < ymax; j++) {
@@ -30,27 +31,21 @@ public class Delivery {
         for (int i = 0; i < xmax; i++)
             for (int j = 0; j < ymax; j++)
                 if (col[i][j] == 'w') {
-                    dfsVisit(i, j);
+                    dfsVisit(i, j, goalx, goaly);
                 }
-        return backtrackPath(xmax - 1, ymax - 1);
     }
 
-    public static void dfsVisit(int x, int y) {
-        //if (map[x][y]) {
-        //    System.out.println("(" + x + ", " + y + ") Hier liegt ein Hindernis.");
-        //    col[x][y] = 's';
-        //    return;
-        //}
+    public static void dfsVisit(int x, int y, int goalx, int goaly) {
         if (col[x][y] != 'w') {
             System.out.println("Zyklus verhindert.");
             return;
         }
-        if (x == xmax - 1 && y == ymax - 1) {
-            System.out.println("Endpunkt erreicht.");
-        }
         col[x][y] = 'g';
-        time++;
-        disc[x][y] = time;
+
+        // This is the end, so return
+        if (x == goalx && y == goaly) {
+            return;
+        }
 
         // For all white neighbours
         // RIGHT
@@ -63,7 +58,7 @@ public class Delivery {
                 prev[x][y + 1][0] = x;
                 prev[x][y + 1][1] = y;
 
-                dfsVisit(x, y + 1);
+                dfsVisit(x, y + 1, goalx, goaly);
             }
         }
         // DOWN
@@ -76,7 +71,7 @@ public class Delivery {
                 prev[x + 1][y][0] = x;
                 prev[x + 1][y][1] = y;
 
-                dfsVisit(x + 1, y);
+                dfsVisit(x + 1, y, goalx, goaly);
             }
         }
         // UP
@@ -89,7 +84,7 @@ public class Delivery {
                 prev[x - 1][y][0] = x;
                 prev[x - 1][y][1] = y;
 
-                dfsVisit(x - 1, y);
+                dfsVisit(x - 1, y, goalx, goaly);
             }
         }
         // LEFT
@@ -102,13 +97,11 @@ public class Delivery {
                 prev[x][y - 1][0] = x;
                 prev[x][y - 1][1] = y;
 
-                dfsVisit(x, y - 1);
+                dfsVisit(x, y - 1, goalx, goaly);
             }
         }
 
         col[x][y] = 's';
-        time++;
-        fin[x][y] = time;
     }
 
     public static String backtrackPath(int fromx, int fromy) {
@@ -209,21 +202,8 @@ public class Delivery {
                 {false, false, false, false, false, false, true, false, false, false},
                 {false, false, false, false, false, false, false, false, false, false}
         };
-        //System.out.println(suche(testmap0));
-        System.out.println(checkPath(testmap2, suche(testmap2)));
-        for (int i = 0; i < xmax; i++) {
-            for (boolean j : testmap0[i]) {
-                if (j) System.out.print("X   ");
-                else System.out.print("O   ");
-            }
-            System.out.println();
-        }
-        for (int i = 0; i < xmax; i++) {
-            for (int j[] : prev[i]) {
-                System.out.print(j[0] + ", ");
-                System.out.print(j[1] + "\t");
-            }
-            System.out.println();
-        }
+        System.out.println(checkPath(testmap0, search(testmap0)));
+        System.out.println(checkPath(testmap1, search(testmap1)));
+        System.out.println(checkPath(testmap2, search(testmap2)));
     }
 }
