cc742384f4cfcf414f7ebce3b5fb1bd588822546
Nils Kuhnert <3c7@outlook.com>
2015-11-17 00:10:14 +0100

    Corrected version

--- src/Delivery.java
+++ src/Delivery.java
@@ -1,6 +1,3 @@
-import java.util.Arrays;
-import java.util.Stack;
-
 /**
  * Created by Nils on 16.11.2015.
  */
@@ -23,50 +20,41 @@ public class Delivery {
         fin = new int[xmax][ymax];
         prev = new int[xmax][ymax][2];
 
+
+        // DFS
         for (int i = 0; i < xmax; i++)
             for (int j = 0; j < ymax; j++) {
                 col[i][j] = 'w';
-                prev[i][j][0] = 0;
-                prev[i][j][1] = 0;
             }
 
         for (int i = 0; i < xmax; i++)
             for (int j = 0; j < ymax; j++)
-                if (col[i][j] == 'w')
+                if (col[i][j] == 'w') {
                     dfsVisit(i, j);
-        return "";
+                }
+        return backtrackPath(xmax - 1, ymax - 1);
     }
 
     public static void dfsVisit(int x, int y) {
-        if (map[x][y]) {
-            System.out.println("(" + x + ", " + y + ") Hier liegt ein Hindernis.");
-            col[x][y] = 's';
-            return;
-        }
+        //if (map[x][y]) {
+        //    System.out.println("(" + x + ", " + y + ") Hier liegt ein Hindernis.");
+        //    col[x][y] = 's';
+        //    return;
+        //}
         if (col[x][y] != 'w') {
             System.out.println("Zyklus verhindert.");
             return;
+        }
+        if (x == xmax - 1 && y == ymax - 1) {
+            System.out.println("Endpunkt erreicht.");
         }
         col[x][y] = 'g';
         time++;
         disc[x][y] = time;
 
         // For all white neighbours
-        // DOWN
-        if (x + 2 < xmax && !map[x + 1][y]) {
-
-            // Recursive call
-            if (col[x + 1][y] == 'w') {
-
-                // Predecessor
-                prev[x + 1][y][0] = x;
-                prev[x + 1][y][1] = y;
-
-                dfsVisit(x + 1, y);
-            }
-        }
         // RIGHT
-        if (y + 2 < ymax && !map[x][y + 1]) {
+        if (y + 1 < ymax && !map[x][y + 1]) {
 
             // Recursive call
             if (col[x][y + 1] == 'w') {
@@ -76,6 +64,19 @@ public class Delivery {
                 prev[x][y + 1][1] = y;
 
                 dfsVisit(x, y + 1);
+            }
+        }
+        // DOWN
+        if (x + 1 < xmax && !map[x + 1][y]) {
+
+            // Recursive call
+            if (col[x + 1][y] == 'w') {
+
+                // Predecessor
+                prev[x + 1][y][0] = x;
+                prev[x + 1][y][1] = y;
+
+                dfsVisit(x + 1, y);
             }
         }
         // UP
@@ -110,16 +111,66 @@ public class Delivery {
         fin[x][y] = time;
     }
 
-   /* public static String backtrackPath(int fromx, int fromy) {
+    public static String backtrackPath(int fromx, int fromy) {
         int x = fromx, y = fromy;
-        while (x > 0 && y > 0) {
+        while (x > 0 || y > 0) {
             int xprev = prev[x][y][0];
             int yprev = prev[x][y][1];
+            if (x - xprev > 0) {
+                path = "d" + path;
+            }
+            if (xprev - x > 0) {
+                path = "u" + path;
+            }
+            if (y - yprev > 0) {
+                path = "r" + path;
+            }
+            if (yprev - y > 0) {
+                path = "l" + path;
+            }
 
 
-
+            x = xprev;
+            y = yprev;
         }
-    }*/
+        return path;
+    }
+
+    public static boolean checkPath(boolean[][] map, String path) {
+        int l = path.length();        // Laenge des Weges
+        int size = map.length;        // Groesse des Labyrinths sowohl in x- als auch in y-Richtung
+        int x = 0;            // aktuelle x-Position des Robots (Zeile)
+        int y = 0;            // aktuelle y-Position des Robots (Spalte)
+        char move;            // einzelne Bewegung des Roboters
+
+        for (int i = 0; i < l; i++) {    // Laufe den Weg des Roboters ab.
+            move = path.charAt(i);    // Bewegung, die der Roboter ausfuehren soll
+            switch (move) {
+                case 'd':
+                    x = x + 1;
+                    break;    // nach unten (down) ==> Zeile + 1
+                case 'u':
+                    x = x - 1;
+                    break;    // nach oben (up) ==> Zeile - 1
+                case 'r':
+                    y = y + 1;
+                    break;    // nach rechts (right) ==> Spalte + 1
+                case 'l':
+                    y = y - 1;
+                    break;    // nach links (left) ==> Spalte - 1
+                default:
+                    return false;        // alle anderen Zeichen duerfen hier nicht auftreten
+            }
+            // Wenn der Roboter aus dem Labyrinth oder
+            // gegen einen Felsen gelaufen ist, dann ist der Weg
+            // ungueltig.
+            if (x < 0 || x >= size || y < 0 || y >= size || map[x][y]) {
+                return false;
+            }
+        }
+        // Wenn man hier angekommen ist, dann ist der Weg gueltig.
+        return x == size - 1 && y == size - 1;    // Pruefe nun, ob die Zielposition erreicht ist.
+    }
 
     public static void main(String[] args) {
         boolean[][] testmap0 = {
@@ -134,18 +185,43 @@ public class Delivery {
                 {false, false, false, false, false, false, false, false, false, false},
                 {false, false, false, false, false, true, false, false, false, false}
         };
-        suche(testmap0);
-        for (int i = 0; i<xmax;i++) {
-            for(boolean j : testmap0[i]) {
-                if(j) System.out.print("X   ");
+        boolean[][] testmap1 = {
+                {false, false, false, false, false, false, false, false, false, false},
+                {false, false, false, false, false, false, false, false, false, false},
+                {false, true, false, false, false, false, false, false, false, false},
+                {false, false, true, false, false, false, true, false, false, false},
+                {false, false, false, false, true, false, false, false, false, false},
+                {false, false, false, true, false, false, false, true, false, false},
+                {false, false, false, false, true, false, false, false, false, true},
+                {false, false, false, false, false, false, false, false, true, false},
+                {false, false, false, false, false, false, false, false, false, false},
+                {false, false, false, true, true, false, true, false, false, false}
+        };
+        boolean[][] testmap2 = {
+                {false, false, false, false, true, true, false, false, true, false},
+                {false, false, false, false, false, false, false, true, false, false},
+                {false, false, true, false, false, true, false, true, false, false},
+                {false, false, true, true, false, false, true, false, true, false},
+                {true, true, false, false, false, false, true, false, false, false},
+                {false, false, false, false, true, false, true, false, true, false},
+                {false, false, true, false, false, false, false, true, false, false},
+                {false, false, false, false, false, true, false, true, false, false},
+                {false, false, false, false, false, false, true, false, false, false},
+                {false, false, false, false, false, false, false, false, false, false}
+        };
+        //System.out.println(suche(testmap0));
+        System.out.println(checkPath(testmap2, suche(testmap2)));
+        for (int i = 0; i < xmax; i++) {
+            for (boolean j : testmap0[i]) {
+                if (j) System.out.print("X   ");
                 else System.out.print("O   ");
             }
             System.out.println();
         }
-        for (int i = 0; i<xmax;i++) {
-            for(int j[] : prev[i]) {
-                System.out.print(j[0]+", ");
-                System.out.print(j[1]+ "\t");
+        for (int i = 0; i < xmax; i++) {
+            for (int j[] : prev[i]) {
+                System.out.print(j[0] + ", ");
+                System.out.print(j[1] + "\t");
             }
             System.out.println();
         }
