70f8abbcdc1c0ecaafa92e64aaec8006d0a3eb3e
3c7 <s3cur3kosmos1001>
2015-11-21 14:27:30 +0100

    Different changes according to match the tests

--- src/Delivery.java
+++ src/Delivery.java
@@ -16,16 +16,30 @@ public class Delivery {
         prev = new int[xmax][ymax][2];
         path = "";
 
-        dfs(0, 0, xmax - 1, ymax - 1);
-
-        return backtrackPath(xmax - 1, ymax - 1);
+        dfs(xmax - 1, ymax - 1);
+        path = backtrackPath(xmax - 1, ymax - 1);
+        return path;
     }
 
-    public static void dfs(int startx, int starty, int goalx, int goaly) {
+    public static void outPath() {
+        for (int arr[][] : prev) {
+            for (int pos[] : arr) {
+                System.out.print(pos[0] + ", " + pos[1] + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void dfs(int goalx, int goaly) {
         // DFS
         for (int i = 0; i < xmax; i++)
             for (int j = 0; j < ymax; j++) {
                 col[i][j] = 'w';
+                if (map[i][j]) {
+                    col[i][j] = 's';
+                    prev[i][j][0] = Integer.MAX_VALUE;
+                    prev[i][j][1] = Integer.MAX_VALUE;
+                }
             }
 
         for (int i = 0; i < xmax; i++)
@@ -71,6 +85,7 @@ public class Delivery {
                 prev[x + 1][y][0] = x;
                 prev[x + 1][y][1] = y;
 
+
                 dfsVisit(x + 1, y, goalx, goaly);
             }
         }
@@ -106,9 +121,23 @@ public class Delivery {
 
     public static String backtrackPath(int fromx, int fromy) {
         int x = fromx, y = fromy;
+        // If end is not reachable, return null
+        // If end is reachable without moving, return empty string
+        if (prev[x][y][0] == 0 && prev[x][y][1] == 0 && map.length > 1) {
+            return null;
+        } else if (prev[x][y][0] == 0 && prev[x][y][1] == 0 && map.length == 1 && !map[x][y]) {
+            return "";
+        }
+
+        // Backtrace starts on blocked coordinate
+        if (map[x][y]) return null;
+
         while (x > 0 || y > 0) {
             int xprev = prev[x][y][0];
             int yprev = prev[x][y][1];
+
+            if (xprev + yprev == 0 && x + y > 1) return null;
+
             if (x - xprev > 0) {
                 path = "d" + path;
             }
@@ -127,83 +156,5 @@ public class Delivery {
             y = yprev;
         }
         return path;
-    }
-
-    public static boolean checkPath(boolean[][] map, String path) {
-        int l = path.length();        // Laenge des Weges
-        int size = map.length;        // Groesse des Labyrinths sowohl in x- als auch in y-Richtung
-        int x = 0;            // aktuelle x-Position des Robots (Zeile)
-        int y = 0;            // aktuelle y-Position des Robots (Spalte)
-        char move;            // einzelne Bewegung des Roboters
-
-        for (int i = 0; i < l; i++) {    // Laufe den Weg des Roboters ab.
-            move = path.charAt(i);    // Bewegung, die der Roboter ausfuehren soll
-            switch (move) {
-                case 'd':
-                    x = x + 1;
-                    break;    // nach unten (down) ==> Zeile + 1
-                case 'u':
-                    x = x - 1;
-                    break;    // nach oben (up) ==> Zeile - 1
-                case 'r':
-                    y = y + 1;
-                    break;    // nach rechts (right) ==> Spalte + 1
-                case 'l':
-                    y = y - 1;
-                    break;    // nach links (left) ==> Spalte - 1
-                default:
-                    return false;        // alle anderen Zeichen duerfen hier nicht auftreten
-            }
-            // Wenn der Roboter aus dem Labyrinth oder
-            // gegen einen Felsen gelaufen ist, dann ist der Weg
-            // ungueltig.
-            if (x < 0 || x >= size || y < 0 || y >= size || map[x][y]) {
-                return false;
-            }
-        }
-        // Wenn man hier angekommen ist, dann ist der Weg gueltig.
-        return x == size - 1 && y == size - 1;    // Pruefe nun, ob die Zielposition erreicht ist.
-    }
-
-    public static void main(String[] args) {
-        boolean[][] testmap0 = {
-                {false, false, false, false, false, false, false, false, false, false},
-                {false, false, false, false, false, false, false, false, false, false},
-                {false, false, false, false, false, false, false, false, true, false},
-                {false, false, false, false, false, false, true, true, false, false},
-                {false, false, false, false, false, false, false, false, false, false},
-                {false, false, false, false, false, false, false, false, false, false},
-                {true, false, false, false, false, false, false, false, true, false},
-                {false, false, false, false, false, false, false, false, true, false},
-                {false, false, false, false, false, false, false, false, false, false},
-                {false, false, false, false, false, true, false, false, false, false}
-        };
-        boolean[][] testmap1 = {
-                {false, false, false, false, false, false, false, false, false, false},
-                {false, false, false, false, false, false, false, false, false, false},
-                {false, true, false, false, false, false, false, false, false, false},
-                {false, false, true, false, false, false, true, false, false, false},
-                {false, false, false, false, true, false, false, false, false, false},
-                {false, false, false, true, false, false, false, true, false, false},
-                {false, false, false, false, true, false, false, false, false, true},
-                {false, false, false, false, false, false, false, false, true, false},
-                {false, false, false, false, false, false, false, false, false, false},
-                {false, false, false, true, true, false, true, false, false, false}
-        };
-        boolean[][] testmap2 = {
-                {false, false, false, false, true, true, false, false, true, false},
-                {false, false, false, false, false, false, false, true, false, false},
-                {false, false, true, false, false, true, false, true, false, false},
-                {false, false, true, true, false, false, true, false, true, false},
-                {true, true, false, false, false, false, true, false, false, false},
-                {false, false, false, false, true, false, true, false, true, false},
-                {false, false, true, false, false, false, false, true, false, false},
-                {false, false, false, false, false, true, false, true, false, false},
-                {false, false, false, false, false, false, true, false, false, false},
-                {false, false, false, false, false, false, false, false, false, false}
-        };
-        System.out.println(checkPath(testmap0, search(testmap0)));
-        System.out.println(checkPath(testmap1, search(testmap1)));
-        System.out.println(checkPath(testmap2, search(testmap2)));
     }
 }
